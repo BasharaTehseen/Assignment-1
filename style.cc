28b018ae1a1117990a7b6126053651d4647b16c5
BasharaTehseen <sp19-bse-032@cuilahore.edu.pk>
2021-10-01 16:22:51 +0500

    Initial commit

---
+++ style.cc
@@ -0,0 +1,91 @@
+#nine
+{ 
+    height: 800px;
+    background-color:white;
+    width: 32%;
+    border-style:solid;
+    border-color: gainsboro;
+    padding: 0px;
+    float: left;
+}
+#pp
+{
+    font-size:20pt;
+    font-style: bold;
+    text-align: center;
+    
+}
+#ten
+{
+    text-align: center;
+}
+#eleven
+{
+   
+    height: 800px;
+    background-color:white;
+    width: 32%;
+    border-style:solid;
+    border-color: gainsboro;
+    padding: 0px;
+    float: left;
+}
+#pt
+{
+    font-size:20pt;
+    font-style: bold;
+    text-align: center;
+    
+}
+#twelve
+{
+   
+    height: 800px;
+    background-color:white;
+    width: 34.5%;
+    border-style:solid;
+    border-color: gainsboro;
+    padding: 0px;
+    float: left;
+}
+#fg
+{
+    font-size:20pt;
+    font-style: bold;
+    text-align: center;
+    
+}
+#text{
+    font: 30pt;
+}
+#black{
+text-align: center;
+}
+#thre{
+    font-size: 20pt;
+    color:black
+}
+#four{
+    font-size: 20pt;
+    color:black
+}
+#five{
+    font-size: 20pt;
+    color:black
+}
+#six{
+    font-size: 20pt;
+    color:black
+}#seven{
+    font-size: 20pt;
+    color:black
+}
+#thrd{
+    font-size: 40 pt;
+    text-align: center;
+}
+
+#track
+{
+    font-size:30px;
+}
